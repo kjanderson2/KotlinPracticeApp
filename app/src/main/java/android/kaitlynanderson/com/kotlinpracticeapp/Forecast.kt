f6cf5318d48c9d2a4c42c81cd5e9b5ff89ed7a53
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2018-02-27 16:41:33 -0800

    added Forecast data class

---
+++ app/src/main/java/android/kaitlynanderson/com/kotlinpracticeapp/Forecast.kt
@@ -0,0 +1,8 @@
+package android.kaitlynanderson.com.kotlinpracticeapp
+
+import java.util.*
+
+/**
+ * Created by kaitlynanderson on 2/27/18.
+ */
+data class Forecast(val date: Date, val temperature: Float, val details: String)
