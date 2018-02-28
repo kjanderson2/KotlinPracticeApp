b41126dfd9964fa6a3963ae4350f509d542fd4a9
Kaitlyn Anderson <kaitlyn.anderson@uncorkedstudios.com>
2018-02-27 16:35:45 -0800

    added api request

--- app/src/main/java/android/kaitlynanderson/com/kotlinpracticeapp/MainActivity.kt
+++ app/src/main/java/android/kaitlynanderson/com/kotlinpracticeapp/MainActivity.kt
@@ -4,18 +4,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val forecastList : RecyclerView = find(R.id.forecast_list)
-        forecastList.layoutManager = LinearLayoutManager(this)
-        forecastList.adapter = ForecastListAdapter(items)
-    }
 
     private val items = listOf(
             "Mon 6/23 - Sunny - 31/17",
@@ -25,4 +19,23 @@ class MainActivity : AppCompatActivity() {
             "Fri 6/27 - Foggy - 21/10",
             "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
             "Sun 6/29 - Sunny - 20/7")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val forecastList = find<RecyclerView>(R.id.forecast_list)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync {
+            Request(url).run()
+            uiThread { longToast("Request performed") }
+        }
+    }
+
+
 }
--- app/src/main/java/android/kaitlynanderson/com/kotlinpracticeapp/Request.kt
+++ app/src/main/java/android/kaitlynanderson/com/kotlinpracticeapp/Request.kt
@@ -1,5 +1,14 @@
 package android.kaitlynanderson.com.kotlinpracticeapp
 
+import android.util.Log
+import java.net.URL
+
 /**
  * Created by kaitlynanderson on 2/27/18.
  */
+class Request(private val url: String) {
+    fun run() {
+        val forecastJsonStr = URL(url).readText()
+        Log.d(javaClass.simpleName, forecastJsonStr)
+    }
+}
